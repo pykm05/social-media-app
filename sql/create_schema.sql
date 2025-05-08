e77d97a7a2ebe0149b52cbec1391ece4f3e2c36a
Daniel Lee <32139397+Daniel556-1@users.noreply.github.com>
2025-05-08 12:52:12 -0700

    Fixed file structure

---
+++ sql/create_schema.sql
@@ -0,0 +1,67 @@
+-- Include a SQL script (create_schema.sql) that can be used to recreate the 
+-- database schema, with all tables, constraints, and any initial data.
+
+-- TABLE CREATION ---------------------------------------------------
+
+CREATE TABLE Users(
+  username VARCHAR(50) PRIMARY KEY UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  date_of_creation DATE NOT NULL,
+  is_admin DEFAULT NULL
+);
+
+CREATE TABLE friends(
+  username1 VARCHAR(50) NOT NULL,
+  username2 VARCHAR(50) NOT NULL,
+  CHECK (username1 <> username2),
+  FOREIGN KEY (username1) REFERENCES users(username),
+  FOREIGN KEY (username2) REFERENCES users(username)
+);
+
+CREATE TABLE friend_requests(
+  sender_username VARCHAR(50) NOT NULL,
+  receiver_username VARCHAR(50) NOT NULL,
+  CHECK (sender_username <> receiver_username),
+  FOREIGN KEY (sender_username) REFERENCES users(username),
+  FOREIGN KEY (receiver_username) REFERENCES users(username)
+);
+
+CREATE TABLE Posts(
+  post_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title VARCHAR(50) NOT NULL,
+  contents VARCHAR(256) NOT NULL,
+  owner VARCHAR(50) NOT NULL,
+  date_of_post DATE NOT NULL,
+  FOREIGN KEY (owner) REFERENCES Users(username)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE Votes(
+  post_id INT NOT NULL,
+  username VARCHAR(50) NOT NULL,
+  numVote INT NOT NULL,
+  FOREIGN KEY (post_id) REFERENCES Posts(post_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  FOREIGN KEY (username) REFERENCES Users(username)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  PRIMARY KEY (post_id, username)
+);
+
+CREATE TABLE comment(
+  comment_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  contents VARCHAR(256) NOT NULL,
+  owner VARCHAR(50) NOT NULL,
+  date_of_post DATE NOT NULL,
+  post_id INT NOT NULL,
+  FOREIGN KEY (owner) REFERENCES Users(username)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  FOREIGN KEY (post_id) REFERENCES Posts(post_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+);
+
+-- DATA INITIALIZATION IN THE OTHER FILE
---
+++ sql/initialize_data.sql
@@ -0,0 +1,13 @@
+-- Provide a script to populate tables with at least 15 entries per table 
+-- (initialize_data.sql), ensuring that all fields are populated and no NULL 
+-- values are present.
+
+-- Commands located in file:
+-- social-media-app/freeform-springboot/src/main/java/com/example/demo/UserDAO.java
+
+-- BACKEND QUERIES ---------------------------------------------------
+
+-- Populate friend_requests
+
+
+
